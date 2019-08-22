1cbb0a60ffd18f57947c09a1514462e97b560511
famaridon <famaridon@users.noreply.github.com>
2019-08-22 15:44:46 +0200

    append empty line in place replace by empty line

--- .travis/setup-env.sh
+++ .travis/setup-env.sh
@@ -6,4 +6,4 @@ PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate 
 echo "#!/usr/bin/env bash" > /$HOME/ci-tools.sh
 echo "PROJECT_ARTIFACT_ID=${PROJECT_ARTIFACT_ID}" >> /$HOME/ci-tools.sh
 echo "PROJECT_VERSION=${PROJECT_VERSION}" >> /$HOME/ci-tools.sh
-echo "" > /$HOME/ci-tools.sh # add empty line at EOF
+echo "" >> /$HOME/ci-tools.sh # add empty line at EOF
